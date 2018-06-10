75c02cbe559757595b8023f1818ff08ccdabd54a
erackley <erackley@malwarebytes.org>
2018-06-10 12:37:36 -0400

    Fixed CPUScanWorker so that it now uses a threadpool to calculate the checksum. This brings it in line with the performance we see for GPUScanWorker, except that CPUScanWorker is using 100% CPU whereas GPUScanWorker maxes out at 40% CPU.

--- OpenCLProofOfConcept/GPUScanWorker.cpp
+++ OpenCLProofOfConcept/GPUScanWorker.cpp
@@ -91,7 +91,7 @@ bool GPUScanWorker::queueScanOperation()
 {
     if(m_filesToScan.size() <= 0 || (m_state != ScanWorkerState::Available && m_state != ScanWorkerState::Ready))
     {
-        qDebug() << Q_FUNC_INFO << "Scan operation called with no files to scan.";
+        //qDebug() << Q_FUNC_INFO << "Scan operation called with no files to scan.";
         return false;
     }
 
@@ -215,7 +215,7 @@ void GPUScanWorker::processResults()
             {
                 emit infectionFound(m_filesToScan.at(index));
             }
-            qDebug() << "Checksum for " << m_filesToScan.at(index) << ": " << m_hostResultData[index];
+            //qDebug() << "Checksum for " << m_filesToScan.at(index) << ": " << m_hostResultData[index];
         }
     }
 }
